6bb5e0f1f8cff3c9e9c175e5241f5a829fbb4198
paweljanuszkiewicz <paweljanuszkiewicz@outlook.com>
2016-10-07 21:38:23 +0200

    font size editing

--- js/script.js
+++ js/script.js
@@ -9,14 +9,26 @@ var howManyBoxes;
 //menu
 menu.querySelector('.add-box').addEventListener('click', addBox);
 menu.querySelector('.remove-all').addEventListener('click', removeAll);
-//boxes
-
+menu.querySelector('.font-size span:first-child').addEventListener('click', incFont);
+menu.querySelector('.font-size span:last-child').addEventListener('click', decFont);
 
 //"MAIN"
 var base = 100;
 howManyBoxes = 0;
+var difFS = 2;
 
 //GLOBAL functions
+function incFont () {
+  var html = document.querySelector('html');
+  var cs = window.getComputedStyle(html, null).getPropertyValue('font-size');
+  document.querySelector('html').style.fontSize = parseInt(cs) + difFS + "px";
+}
+function decFont () {
+  var html = document.querySelector('html');
+  var cs = window.getComputedStyle(html, null).getPropertyValue('font-size');
+  document.querySelector('html').style.fontSize = parseInt(cs) - difFS + "px";
+}
+
 function addEventsBoxes () {
   var boxes = board.querySelectorAll('.box');
   //add events listeners
@@ -27,6 +39,12 @@ function addEventsBoxes () {
     boxes[i].querySelector('.close').addEventListener('mouseout', closeHoverOff);
     boxes[i].querySelector('.close').addEventListener('click', removeBox);
     boxes[i].querySelector('.add').addEventListener('click', addLi);
+    boxes[i].querySelector('h1').addEventListener('click', editLi);
+    boxes[i].querySelector('h1 + input').addEventListener('blur', updateLi);
+    boxes[i].querySelector('h1 + input').addEventListener('keypress', function(e) {
+      if (e.which == 13)
+        updateLi.call(this);
+    });
   }
 }
 
@@ -35,7 +53,7 @@ function addBox () {
   board.innerHTML += '<div class="box">'
     + '<div class="bar">'
       + '<div class="stick-icon"><img src="images/pin-icon.png"></div>'
-      + '<div class="close">x</div>'
+      + '<div class="close"><img src="images/close-icon.png"></div>'
     + '</div>'
     + '<h1>Title</h1><input type="text" value="Title">'
     + '<ul><li class="add"><span>.</span><input type="text"></li></ul></div>';
@@ -45,7 +63,7 @@ function addBox () {
   howManyBoxes++;
   addEventsBoxes();
 }
-function removeAll () {
+function removeAll () { //delete all except .line
   while (board.lastChild) {
     if (board.lastChild.classList.contains('line')) break;
     board.removeChild(board.lastChild);
@@ -68,25 +86,37 @@ function removeBox () {
 }
 function updateLi() {
   this.previousElementSibling.innerHTML = this.value;
+  if (this.previousElementSibling.nodeName == "H1") {
+    this.previousElementSibling.classList.remove('edit');
+    return;
+  }
+  if (this.value.length === 0) {  //if empty remove this
+    removeLi.call(this);
+    return;
+  }
   this.parentNode.classList.remove('edit');
 }
 function editLi () {
   if (!this.classList.contains('edit')) {
     this.classList.add('edit');
     var input = this.querySelector('input');
+    if (!input) input = this.parentNode.querySelector('input'); //h1 edit
     input.focus();
     input.setSelectionRange(input.value.length, input.value.length);
   }
 }
+function removeLi () {
+  this.parentNode.parentNode.removeChild(this.parentNode);
+}
 function addLi () {
   this.querySelector('input').addEventListener('blur', updateLi);
+  this.addEventListener('click', editLi);
+  this.classList.remove('add');
+  this.removeEventListener('click', addLi);
   this.querySelector('input').addEventListener('keypress', function(e) {
     if (e.which == 13)
       updateLi.call(this);
   });
-  this.addEventListener('click', editLi);
-  this.classList.remove('add');
-  this.removeEventListener('click', addLi);
   //add new 'add button'
   var li = document.createElement('li');
   var span = document.createElement('span');
